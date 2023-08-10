539244a29c72398d392b99f27cfd5621d17b7bea
Alexander <sasha0435@gmail.com>
2023-08-10 20:38:47 +0300

    lesson-4(Первое приложение(Eclipse)

---
+++ 1. spring-core/springCore/src/main/java/springCore/ru/alishev/springCourse/TestBean.java
@@ -0,0 +1,19 @@
+package springCore.ru.alishev.springCourse;
+
+public class TestBean {
+	private String name;
+	
+	public TestBean(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
