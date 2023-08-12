caedfb1567f760da471ceb3ac2e009a41c28ba47
Alexander <sasha0435@gmail.com>
2023-08-12 18:11:22 +0300

    lesson-7(внедрение зависимостей. Dependency injection DI) часть-2

--- 1. spring-core/springCore/src/main/java/springCore/ru/alishev/springCourse/MusicPlayer.java
+++ 1. spring-core/springCore/src/main/java/springCore/ru/alishev/springCourse/MusicPlayer.java
@@ -4,11 +4,45 @@ package springCore.ru.alishev.springCourse;
 public class MusicPlayer {
 	private Music music;
 	
+	private String name;
+	private int volume;
+	
 	public MusicPlayer(Music music) {
 		this.music = music;
 	}
-	
+
+	public MusicPlayer() {
+	}
+
 	public void playMusic() {
 		System.out.println("Playing: " + music.getSong());
 	}
+
+	public Music getMusic() {
+		return music;
+	}
+
+	public void setMusic(Music music) {
+		this.music = music;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
+	
+	
+	
 }
