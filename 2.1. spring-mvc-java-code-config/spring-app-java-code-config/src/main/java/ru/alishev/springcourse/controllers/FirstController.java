c7f7472eee6a22c30fdda7d70a0ee0146867c7b7
Alexander <sasha0435@gmail.com>
2023-08-15 12:27:41 +0300

    lesson-20(часть-2)

--- 2.1. spring-mvc-java-code-config/spring-app-java-code-config/src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ 2.1. spring-mvc-java-code-config/spring-app-java-code-config/src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -3,6 +3,7 @@ package ru.alishev.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -21,7 +22,8 @@ public class FirstController {
 	}
 	
 	@GetMapping("/goodbye")
-	public String goodByePage() {
+	public String goodByePage(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname) {
+		System.out.println("Goodbye, " + name + " " + surname);
 		return "first/goodbye";
 	}
 }
