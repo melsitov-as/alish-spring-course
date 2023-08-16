904df3564d2462041c2c9b6e1fabb821f9b75be7
Alexander <sasha0435@gmail.com>
2023-08-16 15:53:34 +0300

    lesson-23

--- spring-mvc-crud-app/src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
+++ spring-mvc-crud-app/src/main/java/ru/alishev/springcourse/controllers/PeopleController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ru.alishev.springcourse.dao.PersonDAO;
+import ru.alishev.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -34,5 +37,29 @@ public class PeopleController {
 		// Получаем одного человека из DAO и передадим на отображение в представление
 		model.addAttribute("person", personDAO.show(id));
 		return "people/show";
+	}
+	
+//	@GetMapping("/new")
+//	public String newPerson(Model model) {
+//		// если мы используем thymeleaf-формы, мы должны передавать в шаблон тот объект,
+//		// для которого эта форма нужна
+//		
+//		model.addAttribute("person", new Person());
+//		return "people/new";
+//	}
+	
+	@GetMapping("/new")
+	public String newPerson(@ModelAttribute("person") Person person) {
+		return "people/new";
+	}
+	
+	// здесь мы должны:
+	// получить данные из формы, создать нового человека,
+	// положить в этого человека данные, которые пришли из формы
+	// этого человека добавить в бд
+	@PostMapping()
+	public String create(@ModelAttribute("person") Person person) {
+		personDAO.save(person);
+		return "redirect:/people"; 
 	}
 }
--- spring-mvc-crud-app/src/main/java/ru/alishev/springcourse/models/Person.java
+++ spring-mvc-crud-app/src/main/java/ru/alishev/springcourse/models/Person.java
@@ -9,6 +9,10 @@ public class Person {
 		this.name = name;
 	}
 
+	public Person() {
+		super();
+	}
+
 	public int getId() {
 		return id;
 	}
